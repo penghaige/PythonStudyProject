1bc95e6ea751b333158e68456592c9092b955260
penghaige <2316801207@qq.com>
2023-05-18 19:09:04 +0800

    优化一些平均值计算

--- JudgeStockDayMA.py
+++ JudgeStockDayMA.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 
 
-def judge(mode, code, start_date, end_date, days, abs_pctChg):
+def judge(mode, code, start_date, end_date, days, abs_pctChg, average_abs_pctChg):
     # 获取K线数据
     rs = bs.query_history_k_data_plus(code,
                                       "date,close,tradestatus,amount,volume,pctChg",
@@ -18,28 +18,46 @@ def judge(mode, code, start_date, end_date, days, abs_pctChg):
     is_large_than_abs_pct_chg = True
     # 是否平均成交数量大于指定值
     is_large_than_average_volume = True
+    # 是否平均涨跌幅大于指定值
+    is_large_than_average_abs_pct_chg = True
     # 总成交数量(股)
     total_volume = 0
     # 平均成交数量(股)
     average_volume = 0
+    # 总的涨跌幅绝对值
+    total_abs_pct_chg = 0
+    # 平均涨跌幅绝对值
+    average_abs_pct_chg = 0
     len = rs.data.__len__()
+    # 进行算平均数的天数
+    average_len = 5
+    less_than_average_len = len
     while (rs.error_code == '0') & rs.next():
         row_data = rs.get_row_data()
         data_list.append(row_data)
-        if row_data[4] != '':
-            total_volume = total_volume + float(row_data[4])
 
         if row_data[2] != '1':
             is_normal_trade_status = False
-        pct_chg = row_data[5]
-        if abs_pctChg is not None and pct_chg is not None and pct_chg != '' and abs(float(pct_chg)) < abs_pctChg:
-            is_large_than_abs_pct_chg = False
 
-    if len > 0:
-        average_volume = total_volume / len
+        if less_than_average_len <= average_len:
+            if row_data[4] != '':
+                total_volume = total_volume + float(row_data[4])
+            pct_chg = row_data[5]
+            if pct_chg is not None and pct_chg != '':
+                abs_pct_chg = abs(float(pct_chg))
+                total_abs_pct_chg = total_abs_pct_chg + abs_pct_chg
+                if abs_pctChg is not None and abs_pct_chg < abs_pctChg:
+                    is_large_than_abs_pct_chg = False
 
+        less_than_average_len = less_than_average_len - 1
+
+    if average_len > 0:
+        average_volume = total_volume / average_len
+        average_abs_pct_chg = total_abs_pct_chg / average_len
 
     if is_normal_trade_status is False or is_large_than_abs_pct_chg is False:
+        return False
+    if average_abs_pctChg is not None and average_abs_pctChg > average_abs_pct_chg:
         return False
 
     df = pd.DataFrame(data_list, columns=rs.fields)
@@ -50,14 +68,21 @@ def judge(mode, code, start_date, end_date, days, abs_pctChg):
         df['ma3'] = df['close'].rolling(window=3).mean()
         last_close = df.iloc[-days:]['close']
         last_ma3 = df.iloc[-days:]['ma3']
-        last_volume = df.iloc[-3:]['volume']
-        for val in last_volume.values:
-            if val < average_volume:
-                is_large_than_average_volume = False
+        last_volume = df.iloc[-average_len:]['volume']
+        last_pct_chg = df.iloc[-average_len:]['pctChg']
+        for index, val in enumerate(last_volume.values):
+            if index >= len - average_len:
+                if float(val) < average_volume:
+                    is_large_than_average_volume = False
+                    break
+
+        for index, val in enumerate(last_pct_chg.values):
+            if abs(float(val)) < average_abs_pct_chg:
+                is_large_than_average_abs_pct_chg = False
                 break
 
         # 判断是否符合条件
-        if is_large_than_average_volume and all(last_close >= last_ma3):
+        if is_large_than_average_volume and is_large_than_average_abs_pct_chg and all(last_close >= last_ma3):
             print(f'{code}, 最近{days}天收盘价都不低于3日线')
             return True
         else:
@@ -68,8 +93,20 @@ def judge(mode, code, start_date, end_date, days, abs_pctChg):
         df['ma5'] = df['close'].rolling(window=5).mean()
         last_close = df.iloc[-days:]['close']
         last_ma5 = df.iloc[-days:]['ma5']
+        last_volume = df.iloc[-average_len:]['volume']
+        last_pct_chg = df.iloc[-average_len:]['pctChg']
+        for index, val in enumerate(last_volume.values):
+            if float(val) < average_volume:
+                is_large_than_average_volume = False
+                break
+
+        for index, val in enumerate(last_pct_chg.values):
+            if abs(float(val)) < average_abs_pct_chg:
+                is_large_than_average_abs_pct_chg = False
+                break
+
         # 判断是否符合条件
-        if all(last_close >= last_ma5):
+        if is_large_than_average_volume and is_large_than_average_abs_pct_chg and all(last_close >= last_ma5):
             print(f'{code}, 最近{days}天收盘价都不低于5日线')
             return True
         else:
@@ -87,7 +124,7 @@ def test():
     # end_date = datetime.datetime.now().strftime('%Y-%m-%d')
     start_date = '2023-03-06'
     end_date = '2023-03-27'
-    judge(5, code, start_date, end_date, 5)
+    judge(5, code, start_date, end_date, 5, None, None)
     # 登出baostock
     bs.logout()
 
